3192300f25cf450d577213176024b99bdf41463b
Malik Hemphill <lazaryr@gmail.com>
2017-07-09 17:59:03 -0400

    refresh

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset='utf-8'>
+      	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+      	<meta name="viewport" content="width=device-width, initial-scale=1">
+      	<meta name="description" content="An API providing you with words from the Dictionary of Obscure Sorrows.">
+        <meta name="keywords" content="dictionary, obscure, sorrows, obscure sorrows, words, compendium, John Koenig">
+        <meta name="author" content="Malik Hemphill">
+        
+        <title>Obscure Sorrows API</title>
+        
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+            }
+            
+            p {
+                display: block;
+                font-family: Helvetica, sans-serif;
+                padding: 20px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <p>The Dictionary of Obscure Sorrows API</p>
+    </body>
+</html>
