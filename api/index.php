22d3b4233808915964a3356ece80777a1ff7da40
Malik Hemphill <lazaryr@gmail.com>
2017-09-19 16:03:06 -0400

    added headers for use by other sites

--- api/index.php
+++ api/index.php
@@ -11,6 +11,14 @@
 //            ->withHeader('Content-type', 'application/json; charset=utf-8');
 //    });
 
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    });
+
 
     // Get container
     $container = $app->getContainer();
